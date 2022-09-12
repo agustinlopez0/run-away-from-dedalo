6b69262f0209a47ce15184a582e167cc3fe39d0d
Julian <juliaximo@gmail.com>
2022-09-11 23:19:14 -0300

    Basic camera follow script

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    
+    void FixedUpdate(){
+        transform.position = Vector3.Lerp(transform.position, target.position, smoothSpeed);
+    }
+}
