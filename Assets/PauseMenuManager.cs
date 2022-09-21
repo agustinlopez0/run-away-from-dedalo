f9225a5100b34b4b128d7c3cd79c173fbca7c7db
Julian <juliaximo@gmail.com>
2022-09-20 22:10:18 -0300

    Pause menu placeholder created

---
+++ Assets/PauseMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public OptionsMenu optionsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Debug.Log("Escape key pressed");
+            optionsMenu.gameObject.SetActive(!optionsMenu.gameObject.activeSelf);
+        }
+        
+    }
+}
