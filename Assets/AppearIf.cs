571f0d6dc0b50640e58851ccc8062e3f42b0bbdb
Julian <juliaximo@gmail.com>
2022-09-22 09:39:15 -0300

    Manual attempt to fix failed merge

---
+++ Assets/AppearIf.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearIf : MonoBehaviour
+{
+    public positionController presionado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(presionado.isActivated == true){
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }else{
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+    }
+}
