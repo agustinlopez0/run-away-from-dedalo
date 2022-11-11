768e8625f81e6318b06fd85af152d3e8487e19d5
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-11-11 13:38:38 -0300

    no funciona la pared nivel 1

---
+++ Assets/hideWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideWall : MonoBehaviour
+{
+
+    public MeshRendered pared;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // mesh = pared.GetComponent<MeshRendered>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider collider){
+        pared.enabled = false;
+    }
+    
+    void OnTriggerExit (Collider collider){
+        pared.enabled = true;
+    }
+
+}
